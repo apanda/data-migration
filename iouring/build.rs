8d947f5686dc6c642d09a42dc817082b594a6424
Aurojit Panda <apanda@cs.nyu.edu>
2023-02-19 12:00:36 -0500

    Initial push

---
+++ iouring/build.rs
@@ -0,0 +1,22 @@
+fn main() {
+    use std::env;
+    use std::path::PathBuf;
+
+    const INCLUDE: &str = r#"
+#include <liburing.h>
+    "#;
+
+    let outdir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindgen::Builder::default().header_contents("include-file.h", INCLUDE)
+        .ctypes_prefix("libc")
+        .prepend_enum_name(false)
+        .derive_default(true)
+        .generate_comments(true)
+        .allowlist_type("io_uring_.*|io_.qring_.*|__kernel_timespec|open_how")
+        .allowlist_var("__NR_io_uring.*|IOSQE_.*|IORING_.*|IO_URING_.*|SPLICE_F_FD_IN_FIXED")
+        .use_core()
+        .generate()
+        .unwrap()
+        .write_to_file(outdir.join("iouring-sys.rs"))
+        .unwrap();
+}
